7bc17e58c14cd896f9553d915f37391eba3a772a
えんがわ釣り <engawatsuri@icloud.com>
2026-08-26 18:35:35 +0900

    add: main

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::env;
+use std::sync::Arc;
+use chrono::Local;
+use chrono::TimeDelta;
+use chrono::Datelike;
+use tokio::process::Command;
+use tokio::sync::Semaphore;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("error: no match cmd");
+        return Ok(());
+    }
+
+    let today = Local::now();
+    let weekday = today.weekday().num_days_from_monday() as i32;
+    let before = (weekday - args[1].parse::<i32>()? + 7) % 7;
+    let mut urls: Vec<String> = Vec::new();
+    let url_tpls: [(i32, &str); 12] = [
+        (1, "http://radiko.jp/#!/ts/TBS/{}000000"), // 空気階段の踊り場
+        (2, "http://radiko.jp/#!/ts/TBS/{}000000"), // アルコ&ピース D.C.GARAGE
+        (2, "http://radiko.jp/#!/ts/TBS/{}010000"), // JUNK 爆笑問題カーボーイ
+        (2, "http://radiko.jp/#!/ts/KBS/{}193000"), // 角田龍平の蛤御門のヘン
+        (2, "http://radiko.jp/#!/ts/TBS/{}220000"), // 佐倉綾音 論理×ロンリー
+        (3, "http://radiko.jp/#!/ts/TBS/{}010000"), // 山里亮太の不毛な議論
+        (3, "http://radiko.jp/#!/ts/LFR/{}030000"), // 佐久間宣行のオールナイトニッポン0(ZERO)
+        (4, "http://radiko.jp/#!/ts/TBS/{}000000"), // ハライチのターン！
+        (4, "http://radiko.jp/#!/ts/LFR/{}010000"), // ナインティナインのオールナイトニッポン
+        (5, "http://radiko.jp/#!/ts/OBC/{}230000"), // 森久保祥太郎・浪川大輔　つまみは塩だけ
+        (6, "http://radiko.jp/#!/ts/LFR/{}010000"), // オードリーのオールナイトニッポン
+    ];
+
+    for url_tpl in url_tpls {
+        let lastest_delta = if weekday == url_tpl.0 {
+            let url_time = url_tpl.1[30..].parse::<i32>()?;
+            let today_time = today.format("%H%M%S").to_string().parse::<i32>()?;
+            if url_time < today_time {
+                continue;
+            } else {
+                7
+            }
+        } else {
+            (weekday - url_tpl.0 + 7) % 7
+        };
+        if lastest_delta <= before {
+            let lastest = today - TimeDelta::days(lastest_delta as i64);
+            urls.push(url_tpl.1.to_string().replace("{}", &lastest.format("%Y%m%d").to_string()));
+        }
+    }
+
+    println!("number of radio: {}", urls.len());
+    let semaphore = Arc::new(Semaphore::new(4));
+    let mut handles = vec![];
+    for url in urls {
+        let sem = Arc::clone(&semaphore);
+        let handle = tokio::spawn(async move {
+            let _permit = sem.acquire().await.unwrap();
+            let output = Command::new("yt-dlp").arg(&url).output().await;
+            match output {
+                Ok(out) if out.status.success() => {
+                    println!("success: {}", url);
+                }
+                Ok(_out) => {
+                    eprintln!("failure: {}", url);
+                }
+                Err(_e) => {
+                    eprintln!("failure: yt-dlp");
+                }
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let _ = handle.await;
+    }
+
+    Ok(())
+}
