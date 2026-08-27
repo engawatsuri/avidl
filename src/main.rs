7adbad5653636aff9ad18f5426f7950452f64fdc
えんがわ釣り <engawatsuri@icloud.com>
2026-08-27 21:42:59 +0900

    add: support youtube

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::process::Command;
 use chrono::Local;
 use chrono::TimeDelta;
 use chrono::Datelike;
+use chrono::Duration;
+use chrono::NativeDateTime;
+use chrono::TimeZone;
+use chrono::Utc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url_tpls: [(i32, &str); 12] = [
@@ -17,7 +21,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         (3, "http://radiko.jp/#!/ts/LFR/{}030000"), // 佐久間宣行のオールナイトニッポン0(ZERO)
         (4, "http://radiko.jp/#!/ts/TBS/{}000000"), // ハライチのターン！
         (4, "http://radiko.jp/#!/ts/LFR/{}010000"), // ナインティナインのオールナイトニッポン
+        (4, "https://www.youtube.com/playlist?list=PLxPYUI5vtD6TNW32Yjl2dYirqIaPJSpYL"), // ドラえもん
+        (4, "https://www.youtube.com/playlist?list=PLr8TEuYjnMZXaq-aPICgBDFgck3BQP_JK"), // 日常組
         (5, "http://radiko.jp/#!/ts/OBC/{}230000"), // 森久保祥太郎・浪川大輔　つまみは塩だけ
+        (5, "https://www.youtube.com/playlist?list=PLxPYUI5vtD6TNW32Yjl2dYirqIaPJSpYL"), // ドラえもん
         (6, "http://radiko.jp/#!/ts/LFR/{}010000"), // オードリーのオールナイトニッポン
     ];
     let mut urls: Vec<String> = Vec::new();
@@ -43,8 +50,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (weekday - url_tpl.0 + 7) % 7
         };
         if lastest_delta <= before {
-            let lastest = today - TimeDelta::days(lastest_delta as i64);
-            urls.push(url_tpl.1.to_string().replace("{}", &lastest.format("%Y%m%d").to_string()));
+            if url_tpl.1.starts_with("http://radiko.jp") {
+                let lastest = today - TimeDelta::days(lastest_delta as i64);
+                urls.push(url_tpl.1.to_string().replace("{}", &lastest.format("%Y%m%d").to_string()));
+            } else if url_tpl.1.starts_with("https://www.youtube.com") {
+                let response = ureq::get(url_tpl.1).call()?.into_reader();
+                let feed = feed_rs::parser::parse(response)?;
+                let start_native = NativeDateTime::new(
+                    today.date_native().sub(Duration::days(lastest_delta)),
+                    chrono::NativeTime::from_hms_opt(0, 0, 0).unwrap()
+                );
+                let start: DateTime<Utc> = Local.from_local_datetime(&start_native).unwrap().with_timezone(&Utc);
+                for entry in feed.entries {
+                    if let Some(time) = entry.published {
+                        if start <= time && time < start + Duration::days(1) {
+                            if let Some(link) = entry.links.first() {
+                                urls.push(link.href);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
